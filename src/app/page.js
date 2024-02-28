6257c733d4ee10bf9790bce388d36090724793de
Wathsala Perera <71954322+jwpx@users.noreply.github.com>
2024-02-29 00:52:12 +0530

    bank update

--- src/app/page.js
+++ src/app/page.js
@@ -1,26 +1,81 @@
-import Image from "next/image";
-
+import Image from 'next/image';
+import avatar from 'D:/reactjs/cypherctf/public/avatar.jpg'; 
+import { 
+  AiFillLinkedin} from 'react-icons/ai';
+import { FaXTwitter } from "react-icons/fa6";
+import web1 from 'D:/reactjs/cypherctf/public/web2.jpg';
+import web2 from 'D:/reactjs/cypherctf/public/web2.jpg';
+import web3 from 'D:/reactjs/cypherctf/public/web2.jpg';
+import web4 from 'D:/reactjs/cypherctf/public/web3.jpg';
+import web5 from 'D:/reactjs/cypherctf/public/web3.jpg';
+import web6 from 'D:/reactjs/cypherctf/public/web3.jpg';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24 bg-gradient-to-r from-cyan-500 to-blue-500">
-      <div className="z-10 max-w-lg w-full items-center justify-between font-mono text-sm lg:flex">
-        <div class="custom-shape-divider-top-1707301947">
-          <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
-            <path d="M0,0V46.29c47.79,22.2,103.59,32.17,158,28,70.36-5.37,136.33-33.31,206.8-37.5C438.64,32.43,512.34,53.67,583,72.05c69.27,18,138.3,24.88,209.4,13.08,36.15-6,69.85-17.84,104.45-29.34C989.49,25,1113-14.29,1200,52.47V0Z" opacity=".25" class="shape-fill"></path>
-            <path d="M0,0V15.81C13,36.92,27.64,56.86,47.69,72.05,99.41,111.27,165,111,224.58,91.58c31.15-10.15,60.09-26.07,89.67-39.8,40.92-19,84.73-46,130.83-49.67,36.26-2.85,70.9,9.42,98.6,31.56,31.77,25.39,62.32,62,103.63,73,40.44,10.79,81.35-6.69,119.13-24.28s75.16-39,116.92-43.05c59.73-5.85,113.28,22.88,168.9,38.84,30.2,8.66,59,6.17,87.09-7.5,22.43-10.89,48-26.93,60.65-49.24V0Z" opacity=".5" class="shape-fill"></path>
-            <path d="M0,0V5.63C149.93,59,314.09,71.32,475.83,42.57c43-7.64,84.23-20.12,127.61-26.46,59-8.63,112.48,12.24,165.56,35.4C827.93,77.22,886,95.24,951.2,90c86.53-7,172.46-45.71,248.8-84.81V0Z" class="shape-fill"></path>
-          </svg>
-        </div>
-        <div class="max-w-lg p-8 mt-16 bg-white rounded-lg shadow-md flex flex-col items-center">
-          <Image src="/image.png" alt="Description of image" width={100} height={100} /> <br />
-          <h1 class="text-4xl font-bold text-center mb-6">Welcome to the OSINT Challenge</h1>
-          <p class="text-lg mb-4">Congratulations! You've made it this far.</p>
-          <p class="text-lg mb-6">Now, your mission is to find the flag.</p>
-          <p class="text-lg mb-8">Submit the flag at <a href="http://www.hi.com" class="text-blue-500 underline">www.hi.com</a>.</p>
-          <p class="text-sm mb-6 text-white">flag&#123;oh_sint_you_found_it&#125;</p>
+    <main className=' bg-neutral-900'>
+      <section className='min-h-screen px-10 md:  px-20 lg:px:40'>
+          <nav className='py-10 mb-12 flex justify-between'>
+            <h1 className='text-white text-xl font-mono'>JENNIE.LEE</h1>
+            <ul className=' flex items-center'>
+              <li><a className=' bg-gradient-to-r from-cyan-600 to-cyan-800 text-white px-4 py-2 rounded-md ml-8' href='https://cypherctf.netlify.app/'>Contact Me</a></li>
+            </ul>
+          </nav>
+          
+            <div className='flex flex-wrap gap-9 text-center lg:py-5 lg:max-w-6xl md:justify-center sm:justify-center m-auto'>
+              <div className='lg:text-left lg:px-20'>
+                <h2 className=' text-5xl py-5 text-cyan-600 font-medium md:text-6xl'>Jennie Lee</h2>
+                <h3 className=' text-xl font-mono py-2 md:text-2xl text-white'> Welcome to Jennie Lee's Photography Blog!</h3>
+                <p className=' text-lg py-5 leading-8 text-cyan-600 md:text-l max-w-xl '>
+                  Hi there! I'm Jennie Lee, an accountant by day and a passionate
+                  travel photographer by night. This blog is where I share my travel
+                  experiences, photography tips, and stunning photos from around the
+                  world.
+                </p>
+                <div className=' text-4xl flex lg:justify-start justify-center gap-16 py-3 text-white'>
+                  <a href="https://www.linkedin.com/in/jethendri/" target="_blank"><AiFillLinkedin /></a>
+                  <a href="https://github.com/jwpx" target="_blank"><FaXTwitter /></a>
+                </div>
+              </div>
+              <div className=' rounded-full w-80 h-80 overflow-hidden mx-auto'>
+                <Image src={avatar}></Image>
+              </div>
+            </div>
+            <div className=' flex flex-wrap gap-10 lg:max-w-6xl lg:px-20 m-auto lg:py-4 py-12 justify-center lg:justify-start'>
+              <div><h3 className=' text-xl text-neutral-900 font-medium'>flag&#123;oh_sint_you_found_it&#125;</h3></div>
+            </div>
+      </section>
+      <div class="custom-shape-divider-bottom-1709113727">
+        <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+            <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" class="shape-fill"></path>
+        </svg>
+      </div>
+
+      <div class=" bg-cyan-200 min-h-screen px-10 md:  px-20 lg:px:40"> 
+        <div className=' md:flex gap-x-16 lg:max-w-6xl m-auto py-20 lg:py-20'>
+            <div><h3 className=' text-3xl py-1 text-cyan-900 font-medium'>MY TRAVELS </h3></div>
+            </div>
+          <div className='flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap'>
+            <div className='basis-1/4 flex-1'>
+              <Image src={web1} className="rounded-lg object-cover" width={'100%'} height={'100%'}/>
+            </div>
+            <div className='basis-1/4 flex-1'>
+              <Image src={web2} className="rounded-lg object-cover" width={'100%'} height={'100%'}/>
+            </div>
+            <div className='basis-1/4 flex-1'>
+              <Image src={web3} className="rounded-lg object-cover" width={'100%'} height={'100%'}/>
+            </div>
+            <div className='basis-1/4 flex-1'>
+              <Image src={web4} className="rounded-lg object-cover" width={'100%'} height={'100%'}/>
+            </div>
+            <div className='basis-1/4 flex-1'>
+              <Image src={web5} className="rounded-lg object-cover" width={'100%'} height={'100%'}/>
+            </div>
+            <div className='basis-1/4 flex-1'>
+              <Image src={web6} className="rounded-lg object-cover" width={'100%'} height={'100%'}/>
+            </div>
         </div>
       </div>
     </main>
+    
   );
 }
